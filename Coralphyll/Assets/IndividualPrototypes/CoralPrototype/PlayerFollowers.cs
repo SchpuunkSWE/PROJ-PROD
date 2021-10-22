9184ab78c197817ee073727bf09f4ed024aa54ac
Molly1191 <argentum.molly@gmail.com>
2021-10-22 17:06:44 +0200

    new coral functionality
    fixat en hel del i korallen, obs ej klart än, obs2 min kod är jättestökig inte hunnit städa, obs3 har kommenterat ut den där vänsterklick-grejen i camera-controller-scriptet

---
+++ Coralphyll/Assets/IndividualPrototypes/CoralPrototype/PlayerFollowers.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerFollowers : MonoBehaviour
+{
+    [SerializeField]
+    private List<Follower> allfollowers;
+
+    private List<Follower> followersToDeposit = new List<Follower>();
+
+    public GameObject currentCoral;
+
+    public bool nearCoral = false;
+
+    public List<Follower> GetAllFollowers ()
+    {
+        return allfollowers;
+    }
+
+
+    //public List<Follower> GetFollowersToDeposit()
+    //{
+    //    return followersToDeposit;
+    //}
+
+    public void TransferFollower(Follower fish)
+    {
+        followersToDeposit.Add(fish);
+        Debug.Log(followersToDeposit.Count);
+    }
+
+    public void DepositFish()
+    {
+        currentCoral.GetComponent<Coral>().ReceiveFish(followersToDeposit);
+        Debug.Log("DepositFish Reached");
+        //currentCoral.GetComponent<Coral>().ReceiveFish(allfollowers);
+    }//När man har deposit:at klart måste followersToDeposit tömmas igen - göra i korallen kanske (?)
+
+    private void Update()
+    {
+        if (nearCoral)
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                Debug.Log("M was pressed!");
+                DepositFish();
+            }
+        }
+
+    }
+
+}
+
+
+//Eventuellt: Ändra från lista av Follower till lista av gameobjects och när man behöver grejer i follower,
+//gör isf gamobject.getcomponent<Follower>().blabla
---
+++ Coralphyll/Assets/IndividualPrototypes/CoralPrototype/UI_Trigger.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Trigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject panel;
+
+    private void Awake()
+    {
+        panel.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        if(other.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerFollowers>().nearCoral = true;
+            //Activate Coral UI Panel
+            panel.SetActive(true);
+
+            //Fetch all player's followers
+            List<Follower> playerFollowers = other.GetComponent<PlayerFollowers>().GetAllFollowers();
+
+            Debug.Log("Trigger Entered!");
+
+            //Make all player's followers clickable
+            setClickable(playerFollowers);
+
+        }
+
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerFollowers>().nearCoral = false;
+            panel.SetActive(false);
+            //Debug.Log("Trigger Exited!");
+            //Sätt även spelarens fiskar till non-clickable
+            setClickable(other.GetComponent<PlayerFollowers>().GetAllFollowers());
+        }
+    }
+
+    private void setClickable(List<Follower> followers)
+    {     
+        //Set every fish in the players followers to clickable
+        foreach(Follower fish in followers)
+        {
+            if (!fish.isClickable)
+            {
+                fish.isClickable = true;
+                Debug.Log("I was made clickable!");
+            }
+            else
+            {
+                fish.isClickable = false;
+                Debug.Log("I was made non-clickable!");
+            }
+            //fish.isClickable = true;
+            //Debug.Log(fish.GetInstanceID()+ " Says: I was made clickable!");
+        }
+    }
+
+}
