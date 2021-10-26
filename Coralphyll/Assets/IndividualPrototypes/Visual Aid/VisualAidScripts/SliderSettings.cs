670bde61c8c6928342b394f01499c542e0a88d4f
Yumidun <Josephson.hanna@gmail.com>
2021-10-26 21:03:38 +0200

    fff

--- Coralphyll/Assets/IndividualPrototypes/Visual Aid/VisualAidScripts/SliderSettings.cs
+++ Coralphyll/Assets/IndividualPrototypes/Visual Aid/VisualAidScripts/SliderSettings.cs
@@ -2,22 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
 
-namespace TBOB
+
+public class SliderSettings : MonoBehaviour
 {
-    [System.Serializable]
-    public class SliderSettings
+    public Slider slider;
+    public Text txtSlider;
+    [Tooltip("The maximum value for the settings. It's not viewable for the player")]
+    public float maxSettingsValue;
+    [Tooltip("The minimum value for the settings. It's not viewable for the player")]
+    public float minSettingsValue;
+
+    public Volume volume;
+    VolumeProfile volumeProfile;
+    public bool enableFog;
+    public bool overrideFog;
+
+    void Start()
     {
-        public Slider slider;
-        public Text txtSlider;
-        [Tooltip("The maximum value for the settings. It's not viewable for the player")]
-        public float maxSettingsValue;
-        [Tooltip("The minimum value for the settings. It's not viewable for the player")]
-        public float minSettingsValue;
-
-        public GameObject postProcess; 
-
+       volumeProfile = volume.sharedProfile;
     }
-     
+
+    private void Update()
+    {
+        
+    }
+
+    void Exposure()
+    {
+
+    } 
+
+    void Fog()
+    {
+        if (!volumeProfile.TryGet<Fog>(out var fog))
+        {
+            fog = volumeProfile.Add<Fog>(false);
+        }
+
+        fog.enabled.overrideState = overrideFog;
+        fog.enabled.value = enableFog;
+    }
+
 }
