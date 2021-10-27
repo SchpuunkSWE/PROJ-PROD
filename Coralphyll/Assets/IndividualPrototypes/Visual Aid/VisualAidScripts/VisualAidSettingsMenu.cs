1e7ecb7f1a083311dfaaf1752216d402a92bee51
Yumidun <Josephson.hanna@gmail.com>
2021-10-27 16:13:27 +0200

    set upp en template för koden till menyn

---
+++ Coralphyll/Assets/IndividualPrototypes/Visual Aid/VisualAidScripts/VisualAidSettingsMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualAidSettingsMenu : MonoBehaviour
+{
+    public void SetExposure (float exposure)
+    {
+        //To see the exposure value in the console
+        //Debug.Log(exposure); 
+
+        //Get Volume
+        //Change the exposure according to the value
+    }
+
+    public void SetFog (bool isFog)
+    {
+        //set fog according to the toggle
+    }
+
+    public void SetDof (bool isDof)
+    {
+        //set dof according to the toggle
+    }
+
+    public void SetHighContrastMode(bool isHighContrastMode)
+    {
+        //set highcontrastmode according to the toggle
+    }
+
+    public void ColorPicker()
+    {
+        //pick the colors of important game objects
+    }
+}
