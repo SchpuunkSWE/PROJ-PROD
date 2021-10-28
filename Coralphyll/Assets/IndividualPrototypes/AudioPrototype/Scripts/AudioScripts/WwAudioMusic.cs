f6c56b6dbb281e1f25457628cb141a44211665ed
mr-numbers <carl.herkommer@me.com>
2021-10-28 17:35:12 +0200

    Prototype prep
    Audio prototype prepped for dev

--- Coralphyll/Assets/IndividualPrototypes/AudioPrototype/Scripts/AudioScripts/WwAudioMusic.cs
+++ Coralphyll/Assets/IndividualPrototypes/AudioPrototype/Scripts/AudioScripts/WwAudioMusic.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class WwAudioMusic : MonoBehaviour
 {
     public string wwise_stateGroup_gameState = "MusicState_Initiate";
+    public string event_background_ambience = "Background_Ambience";
    // public string MusicPlay = "Music_initiate"; 
     // Start is called before the first frame update
     void Start()
     {
         AkSoundEngine.RegisterGameObj(gameObject);
-       // AkSoundEngine.PostEvent(MusicPlay, gameObject);
+        // AkSoundEngine.PostEvent(MusicPlay, gameObject);
         AkSoundEngine.PostEvent(wwise_stateGroup_gameState, gameObject);
+        AkSoundEngine.PostEvent(event_background_ambience, gameObject);
     }
     
 }
