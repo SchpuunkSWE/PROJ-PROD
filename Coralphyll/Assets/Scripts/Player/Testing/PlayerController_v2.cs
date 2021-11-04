2f02f7d49704239dccfff8d3dd3c6230364e3572
Filip Hallberg <75782018+SchpuunkSWE@users.noreply.github.com>
2021-11-04 15:09:27 +0100

    Updated the controls so that they are more smooth

--- Coralphyll/Assets/Scripts/Player/Testing/PlayerController_v2.cs
+++ Coralphyll/Assets/Scripts/Player/Testing/PlayerController_v2.cs
@@ -25,24 +25,25 @@ public class PlayerController_v2 : MonoBehaviour
 
     private void Move()
     {
-        rb.velocity = transform.forward * data.thrustAmount * (Mathf.Max(data.thrustInput,0));
+        rb.velocity = transform.forward * data.thrustAmount * data.thrustInput;
     }
 
     private void Turn()
     {
+        float turnVelocity = rb.angularVelocity.y;
         Vector3 newTorque = new Vector3(0, -data.steeringInput.z * data.yawSpeed, 0); //transform.right * Input.GetAxisRaw("Horizontal") + transform.forward * Input.GetAxisRaw("Vertical");
-        if (data.turning)
+        if (data.turning && Mathf.Abs(turnVelocity) < 2)
         {
-            rb.AddRelativeTorque(newTorque);
+           rb.AddRelativeTorque(newTorque);
         }
         else
         {
-            if(rb.angularVelocity.y > 0.2f)
+            if(turnVelocity > 0.1f)
                 rb.AddRelativeTorque(Vector3.down * 0.5f);
-            if (rb.angularVelocity.y < -0.2f)
+            if (turnVelocity < -0.1f)
                 rb.AddRelativeTorque(Vector3.up * 0.5f);
-            if (rb.angularVelocity.y < 0.1f && rb.angularVelocity.y > -0.1f)
-                rb.angularVelocity = Vector3.zero;
+            if (Mathf.Abs(turnVelocity) < 0.1f)
+                rb.angularVelocity = Vector3.Lerp(rb.angularVelocity, Vector3.zero, 0.5f);
         }
 
         rb.rotation = Quaternion.Slerp(rb.rotation, Quaternion.Euler(new Vector3(0, transform.localEulerAngles.y, 0)), 0.5f); //WARNING: DANGER ZONE
