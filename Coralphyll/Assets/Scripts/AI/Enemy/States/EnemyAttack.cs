357e705e90a54871de6baacac1d4db5d74fec796
electfall70 <llop2000@gmail.com>
2021-11-05 01:24:18 +0100

    Fixed basic enemies :) Small fixes

--- Coralphyll/Assets/Scripts/AI/Enemy/States/EnemyAttack.cs
+++ Coralphyll/Assets/Scripts/AI/Enemy/States/EnemyAttack.cs
@@ -15,12 +15,15 @@ public class EnemyAttack : EnemyState
     {
         base.Enter();
         currentCooldown = cooldown;
+        AIController.Renderer.material.color = Color.red;
         //AIController.Animator.SetBool("Attacking", true);
     }
 
     public override void HandleUpdate()
     {
         base.HandleUpdate();
+        RotateTowards(AIController.Player.transform);
+
         //set destination to player
         if (DistanceToPlayer() < attackDistance && attacking == false)
             Attack();
