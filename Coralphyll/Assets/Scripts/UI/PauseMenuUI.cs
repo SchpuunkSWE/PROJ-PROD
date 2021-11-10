75db7135e1f6888296905fece51f832bc8b3b990
MyKarlen <Myy.karlen@gmail.com>
2021-11-10 14:37:26 +0100

    Added Script Pause menu, made ingame prefab,
    Eventual problems: Timescale.delta

---
+++ Coralphyll/Assets/Scripts/UI/PauseMenuUI.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuUI : MonoBehaviour
+{
+    public static bool GameisPaused = false;
+    public GameObject pauseMenuUI;
+
+    public GameObject[] otherPanels;
+
+    public GameObject optionsPanel;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameisPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+
+        }
+    }
+    
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+    }
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void Save(){
+
+    }
+    public void Options(){
+        optionsPanel.SetActive(true);
+        pauseMenuUI.SetActive(false);
+
+
+    }
+
+    
+}
