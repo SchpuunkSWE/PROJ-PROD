e6be3921a19a92716fe95b0f330b50c63a07464d
Angelina Grujic Sjödin <63399967+Kexmannen@users.noreply.github.com>
2021-11-11 23:31:20 +0100

    Did scripts

--- Coralphyll/Assets/IndividualPrototypes/MemoryAidPrototype/MemoryAidScripts/MemoryAidScript.cs
+++ Coralphyll/Assets/IndividualPrototypes/MemoryAidPrototype/MemoryAidScripts/MemoryAidScript.cs
@@ -5,22 +5,48 @@ using UnityEngine;
 public class MemoryAidScript : MonoBehaviour
 {
 
-    GameObject ControlsPanel, ObjectivePanel;
-    // Start is called before the first frame update
+    public GameObject ControlsPanel, ObjectivePanel;
+    public bool ObjectivesPanelActive = false;
+    public bool ControlsPanelsActive = false;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
 
+        //Opening the ObjectivesPanel
         if (Input.GetKeyDown(KeyCode.L))
         {
-            Debug.Log ("L Key was Pressed");
+            ObjectivesPanelActive = !ObjectivesPanelActive;
+            //if L Key pressed is True it will set it to false And the other way around.
 
+            Time.timeScale = ObjectivesPanelActive?0:1;
+            ObjectivePanel.SetActive(ObjectivesPanelActive); // if the booelan is true
+
+        }   
+
+ 
+
+   //Opening the ControlsPanel
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+            ControlsPanelsActive = !ControlsPanelsActive;
+            //if L Key pressed is True it will set it to false And the other way around.
+
+            Time.timeScale = ControlsPanelsActive ? 0:1;
+            ControlsPanel.SetActive(ControlsPanelsActive); // if the booelan is true
         }
+
+
+
+
+    }
         
     }
-}
+
