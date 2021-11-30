48e29b17cf96474bb576e937fda41cbe07dc2be1
electfall70 <llop2000@gmail.com>
2021-11-30 21:09:02 +0100

    Update EnemyChase.cs

--- Coralphyll/Assets/Scripts/AI/Enemy/States/EnemyChase.cs
+++ Coralphyll/Assets/Scripts/AI/Enemy/States/EnemyChase.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "EnemyState/ChaseState")]
 public class EnemyChase : EnemyState
 {
+    [SerializeField] private float detectionRange = 10f;
+    [SerializeField] private float raycastOffset = 1.5f;
+    [SerializeField] private LayerMask collisionMask;
+
     [SerializeField] private float baseChaseSpeed = 5;
     [SerializeField] private float fishFactor = .5f;
     [SerializeField] private float checkCooldown = 2;
@@ -22,11 +26,19 @@ public class EnemyChase : EnemyState
     public override void HandleUpdate()
     {
         base.HandleUpdate();
-        //Set destination to player
-        AIController.transform.position = Vector3.MoveTowards(AIController.transform.position, AIController.Player.transform.position, baseChaseSpeed * (1 + fishFactor * fishAmount) * Time.deltaTime);
-        RotateTowards(AIController.Player.transform);
         HandleCooldown();
+
+        //Set destination to player
+        PathFinding();
+        Move();
+        //AIController.transform.position = Vector3.MoveTowards(AIController.transform.position, AIController.Player.transform.position, baseChaseSpeed * (1 + fishFactor * fishAmount) * Time.deltaTime);
     }
+
+    private void Move()
+    {
+        AIController.transform.position += AIController.transform.forward * baseChaseSpeed * (1 + fishFactor * fishAmount) * Time.deltaTime;
+    }
+
 
     public override void EvaluateTransitions()
     {
@@ -60,5 +72,38 @@ public class EnemyChase : EnemyState
             NPCFishUtil fishUtil = AIController.Player.GetComponent<NPCFishUtil>();
             fishAmount = fishUtil.getListOfFishes().Count;
         }
+    }
+    
+
+    //This is pretty broken sadly :(
+    private void PathFinding()
+    {
+        RaycastHit hit;
+        Vector3 offset = Vector3.zero;
+
+        Vector3 left = AIController.transform.position - AIController.transform.right * raycastOffset;
+        Vector3 right = AIController.transform.position + AIController.transform.right * raycastOffset;
+        Vector3 up = AIController.transform.position + AIController.transform.up * raycastOffset;
+        Vector3 down = AIController.transform.position - AIController.transform.up * raycastOffset;
+
+        Debug.DrawRay(left, AIController.transform.forward * detectionRange, Color.red);
+        Debug.DrawRay(right, AIController.transform.forward * detectionRange, Color.red);
+        Debug.DrawRay(up, AIController.transform.forward * detectionRange, Color.red);
+        Debug.DrawRay(down, AIController.transform.forward * detectionRange, Color.red);
+
+        if (Physics.Raycast(left, AIController.transform.forward, out hit, detectionRange, collisionMask))
+            offset += Vector3.right;
+        else if (Physics.Raycast(right, AIController.transform.forward, out hit, detectionRange, collisionMask))
+            offset -= Vector3.right;
+
+        if (Physics.Raycast(up, AIController.transform.forward, out hit, detectionRange, collisionMask))
+            offset += Vector3.up;
+        else if (Physics.Raycast(down, AIController.transform.forward, out hit, detectionRange, collisionMask))
+            offset -= Vector3.up;
+
+        if (offset != Vector3.zero)
+            AIController.transform.Rotate(offset * 5f * Time.deltaTime);
+        else
+            RotateTowards(AIController.Player.transform);
     }
 }
