5c6f9ae44ba9386e52bb1178c3c8af946c5c0f55
Hampus424 <64363889+Hampus424@users.noreply.github.com>
2021-12-10 15:25:26 +0100

    Kinda fixed sound on bonk
    Spams the fishEat sound when bonking, needs another sound later

--- Coralphyll/Assets/IndividualPrototypes/TerrainPrototype/Scripts/BonkController.cs
+++ Coralphyll/Assets/IndividualPrototypes/TerrainPrototype/Scripts/BonkController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using XInputDotNetPure;
+using static Audio_Events;
 
 public class BonkController : MonoBehaviour
 {
@@ -12,11 +13,17 @@ public class BonkController : MonoBehaviour
 
     public GameObject indicator;
 
+    private GameObject Thing1;
+
 
     PlayerIndex playerIndex;
     GamePadState state;
     GamePadState prevState;
 
+    private void Awake()
+    {
+        AkSoundEngine.RegisterGameObj(gameObject);
+    }
 
     void Start()
     {
@@ -28,18 +35,21 @@ public class BonkController : MonoBehaviour
         audioSource.PlayOneShot(bonk1);
 
         indicator.SetActive(false);
+
+        Thing1 = GameObject.FindGameObjectWithTag("Player");
+
+
     }
 
     void Update()
     {
-        //if (Input.GetKeyDown("joystick button 0"))
         if(overlapping)
         {
             Debug.Log("START VIBRATE");
             GamePad.SetVibration(playerIndex, .1f, .1f);
+            AkSoundEngine.PostEvent("FishEat", gameObject);
         }
 
-        //if (Input.GetKeyDown("joystick button 1"))
         if(!overlapping)
         {
             GamePad.SetVibration(playerIndex, .0f, .0f);
@@ -49,17 +59,17 @@ public class BonkController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.LogError("Start");
+        //Debug.LogError("Start");
         if (other.gameObject.layer == LayerMask.NameToLayer("Collidable"))
         {
             overlapping = true;
-            Debug.Log("Enter");
+            //Debug.Log("Enter");
 
             indicator.SetActive(true);
             audioSource.PlayOneShot(bonk1);
         }
-        else
-            Debug.Log(other.gameObject.layer);
+        //else
+            //Debug.Log(other.gameObject.layer);
     }
 
     private void OnTriggerExit(Collider other)
@@ -67,7 +77,7 @@ public class BonkController : MonoBehaviour
         if (other.gameObject.layer == LayerMask.NameToLayer("Collidable"))
         {
             overlapping = false;
-            Debug.Log("Enter FALSE");
+           // Debug.Log("Enter FALSE");
             indicator.SetActive(false);
         }
     }
