d8202335f8dfce4f85f6fd4ded7ce9868fa4a2e0
Merlime <matilda.eklund96@gmail.com>
2021-12-14 01:21:53 +0100

    försöker nå senaste scenen via getSceneAt

--- Coralphyll/Assets/Scripts/System/SavingSystem/SaveUI.cs
+++ Coralphyll/Assets/Scripts/System/SavingSystem/SaveUI.cs
@@ -2,20 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SaveUI : MonoBehaviour
 {
+    public int level;
+
+
     [SerializeField] GameObject autosaveUI;
     public static bool isAutoSaving = false;
-    //[SerializeField] Animation anim;
     
     [SerializeField] Animator animator;
-
     [SerializeField] RectTransform fxHolder;
     [SerializeField] Image AutoSaveCircle;
+    
+    [SerializeField] [Range(0, 1)] float progress = 0;
+
+    //[SerializeField] Animation anim;
     //[SerializeField] Text txtProgress;
 
-    [SerializeField] [Range(0, 1)] float progress = 0;
     void Start()
     {
         //anim = gameObject.GetComponent<Animation>();
@@ -24,15 +29,34 @@ public class SaveUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //level = SceneManager.GetActiveScene().buildIndex;
+        //level = SceneManager.GetSceneAt().buildIndex;
+        string output = "";
+        for (int n = 0; n < SceneManager.sceneCount; ++n)
+        {
+            Scene scene = SceneManager.GetSceneAt(n);
+            output += scene.name;
+            output += scene.isLoaded ? " (Loaded, " : " (Not Loaded, ";
+            //output += scene.isDirty ? "Dirty, " : "Clean, ";
+            output += scene.buildIndex >= 0 ? " in build)\n" : " NOT in build)\n";
+        }
+
+
+
+
+
+
         AutoSaveCircle.fillAmount = progress;
         //txtProgress.text = Mathf.Floor(progress * 100).ToString();
         fxHolder.rotation = Quaternion.Euler(new Vector3(0f, 0f, -progress * 360));
+        
         //animator.SetBool("isAutoSaving", false);
         
         if(isAutoSaving)
         {
             PlaySavingAnim();
         }
+
     }
 
     public void PlaySavingAnim()
