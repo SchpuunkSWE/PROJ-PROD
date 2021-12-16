7eeb4bb5e8d540b4827ea4cd83f2656a288c0ac1
SchpuunkSWE <75782018+SchpuunkSWE@users.noreply.github.com>
2021-12-16 22:01:04 +0100

    Update ButtonUpdater.cs

--- Coralphyll/Assets/ButtonUpdater.cs
+++ Coralphyll/Assets/ButtonUpdater.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ButtonUpdater : MonoBehaviour
 {
@@ -17,21 +18,21 @@ public class ButtonUpdater : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        buttons[0].GetComponentInChildren<Text>().text = keybindings.keybindingsChecks[2].keyCode.ToString
+        buttons[0].GetComponent<TMPro.TextMeshPro>().text = keybindings.keybindingsChecks[2].keyCode.ToString
         ();
-        buttons[1].GetComponentInChildren<Text>().text = keybindings.keybindingsChecks[5].keyCode.ToString
+        buttons[1].GetComponent<TMPro.TextMeshPro>().text = keybindings.keybindingsChecks[5].keyCode.ToString
         ();
-        buttons[2].GetComponentInChildren<Text>().text = keybindings.keybindingsChecks[1].keyCode.ToString
+        buttons[2].GetComponent<TMPro.TextMeshPro>().text = keybindings.keybindingsChecks[1].keyCode.ToString
         ();
-        buttons[3].GetComponentInChildren<Text>().text = keybindings.keybindingsChecks[7].keyCode.ToString
+        buttons[3].GetComponent<TMPro.TextMeshPro>().text = keybindings.keybindingsChecks[7].keyCode.ToString
         ();
-        buttons[5].GetComponentInChildren<Text>().text = keybindings.keybindingsChecks[3].keyCode.ToString
+        buttons[5].GetComponent<TMPro.TextMeshPro>().text = keybindings.keybindingsChecks[3].keyCode.ToString
         ();
-        buttons[6].GetComponentInChildren<Text>().text = keybindings.keybindingsChecks[4].keyCode.ToString
+        buttons[6].GetComponent<TMPro.TextMeshPro>().text = keybindings.keybindingsChecks[4].keyCode.ToString
         ();
-        buttons[7].GetComponentInChildren<Text>().text = keybindings.keybindingsChecks[0].keyCode.ToString
+        buttons[7].GetComponent<TMPro.TextMeshPro>().text = keybindings.keybindingsChecks[0].keyCode.ToString
         ();
-        buttons[8].GetComponentInChildren<Text>().text = keybindings.keybindingsChecks[6].keyCode.ToString
+        buttons[8].GetComponent<TMPro.TextMeshPro>().text = keybindings.keybindingsChecks[6].keyCode.ToString
         ();
     }
 }
