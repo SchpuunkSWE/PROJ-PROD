34775bdc5cbb7d4f923cbdde6ecd9a3774326ec9
Saheel <saheel96@gmail.com>
2022-01-03 16:26:13 +0100

    fixed frejs enemyoutline to look at all 3 sharks
    causes same problem as my offscreen indicators as the script is searching for objects that are not there in the array

--- Coralphyll/Assets/Scripts/UI/OptionsMenu.cs
+++ Coralphyll/Assets/Scripts/UI/OptionsMenu.cs
@@ -66,15 +66,19 @@ public class OptionsMenu : MonoBehaviour
     public void EnemyOutline(bool enemyOutline)
     {
         PlayerPrefs.SetInt("EnemyOutline", BoolToInt(enemyOutline));
-        GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+        GameObject[] enemy = GameObject.FindGameObjectsWithTag("Enemy");
 
         if (enemyOutlineToggle.GetComponent<Toggle>().isOn)
         {
-            enemy.transform.GetChild(0).transform.GetChild(1).transform.GetChild(2).gameObject.SetActive(true);
+            enemy[0].transform.GetChild(0).transform.GetChild(1).transform.GetChild(2).gameObject.SetActive(true);
+            enemy[1].transform.GetChild(0).transform.GetChild(1).transform.GetChild(2).gameObject.SetActive(true);
+            enemy[2].transform.GetChild(0).transform.GetChild(1).transform.GetChild(2).gameObject.SetActive(true);
         }
         else
         {
-            enemy.transform.GetChild(0).transform.GetChild(1).transform.GetChild(2).gameObject.SetActive(false);
+            enemy[0].transform.GetChild(0).transform.GetChild(1).transform.GetChild(2).gameObject.SetActive(false);
+            enemy[1].transform.GetChild(0).transform.GetChild(1).transform.GetChild(2).gameObject.SetActive(false);
+            enemy[2].transform.GetChild(0).transform.GetChild(1).transform.GetChild(2).gameObject.SetActive(false);
         }
 
     }
