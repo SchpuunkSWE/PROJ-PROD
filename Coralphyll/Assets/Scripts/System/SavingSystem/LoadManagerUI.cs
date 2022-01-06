3127017fad50e45c29f7ddfeae0dd87d905f98fb
Merlime <matilda.eklund96@gmail.com>
2022-01-06 16:27:27 +0100

    Play i load manager funkar nuuu

--- Coralphyll/Assets/Scripts/System/SavingSystem/LoadManagerUI.cs
+++ Coralphyll/Assets/Scripts/System/SavingSystem/LoadManagerUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadManagerUI : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class LoadManagerUI : MonoBehaviour
     [SerializeField] GameObject level3UI;
     [SerializeField] GameObject level4UI;
 
-
+    public static int sceneCount;
 
 
     // Start is called before the first frame update
@@ -19,37 +20,68 @@ public class LoadManagerUI : MonoBehaviour
         
     }
 
+
+    public void LoadGame()
+    {
+        SaveSystem.LoadGame();
+        //level = data.level;
+        //level = SaveSystem.data.level;
+
+        //SaveData data = SaveSystem.data;
+        //data.level = 
+    }
+
+
+
+
     // Update is called once per frame
     void Update()
     {
-        SaveData data = SaveSystem.data;
+        
         //data.level = level;
+        
+        if (sceneCount > 0)
+        {
+            SaveData data = SaveSystem.data;
 
-        if (data.level == 1)
-        {
-            level1UI.SetActive(true);
-            level2UI.SetActive(false);
-            level3UI.SetActive(false);
-            level4UI.SetActive(false);
-        } else if (data.level == 2)
-        {
-            level2UI.SetActive(true);
-            level1UI.SetActive(false);
-            level3UI.SetActive(false);
-            level4UI.SetActive(false);
-        } else if (data.level == 3)
-        {
-            level3UI.SetActive(true);
-            level1UI.SetActive(false);
-            level2UI.SetActive(false);
-            level4UI.SetActive(false);
-        } else if (data.level == 4)
-        {
-            level4UI.SetActive(true);
-            level1UI.SetActive(false);
-            level2UI.SetActive(false);
-            level3UI.SetActive(false);
+            if (data.level == 1)
+            {
+                level1UI.SetActive(true);
+                level2UI.SetActive(false);
+                level3UI.SetActive(false);
+                level4UI.SetActive(false);
+            }
+            else if (data.level == 2)
+            {
+                level2UI.SetActive(true);
+                level1UI.SetActive(false);
+                level3UI.SetActive(false);
+                level4UI.SetActive(false);
+            }
+            else if (data.level == 3)
+            {
+                level3UI.SetActive(true);
+                level1UI.SetActive(false);
+                level2UI.SetActive(false);
+                level4UI.SetActive(false);
+            }
+            else if (data.level == 4)
+            {
+                level4UI.SetActive(true);
+                level1UI.SetActive(false);
+                level2UI.SetActive(false);
+                level3UI.SetActive(false);
+            } else
+            {
+                level1UI.SetActive(false);
+                level2UI.SetActive(false);
+                level3UI.SetActive(false);
+                level4UI.SetActive(false);
+            }
+
         }
+
+        
 
 
     }
